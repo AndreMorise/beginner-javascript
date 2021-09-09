4d09b589ecb09a7121574681d132a70cfc7c9b3d
AndreMorise <andre.morise@gmail.com>
2021-09-09 17:54:47 -0400

    module 1

---
+++ playground/some.js
@@ -0,0 +1,11 @@
+const p = document.querySelector('p');
+console.log('im in another file');
+
+const age = 100;
+console.log(p, age);
+
+const person = {
+  first: 'wes',
+  last: 'bos',
+  age: 100,
+};
---
+++ playground/types.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const first = 'andre';
+const middle = "vance";
+const last = `morise`;
+
+const sentence = `she's so "cool"`;
+
+// const hello = "my name is " + first;
+
+// let hello2 = 'my name is ';
+// hello2 = hello2 + first;
+
+let hello3 = `hello my name is ${first}`;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kids gets ${eachKidGets}`);
+console.log(`Dad gets ${dadGets}`);
